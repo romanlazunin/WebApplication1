6c4ebe31ff3ac4023aa9e795edd527f1c0d131bb
Roman Lazunin <rlazunin@gmail.com>
2016-08-30 10:26:06 +0200

    javascript, events, scope, IIFE

---
+++ src/WebApplication1/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// site.js
+(function (name) {
+
+    var ele = document.getElementById("username");
+    ele.innerHTML = name;
+
+    var main = document.getElementById("main");
+    main.onmouseenter = function () {
+        main.style.backgroundColor = "#888";
+    };
+
+    main.onmouseleave = function () {
+        main.style.backgroundColor = "";
+    };
+})("Roman Lazunin");
