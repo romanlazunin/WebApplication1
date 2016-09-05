a46c6fc04ffb921b5a2b24828f2d3043297496cb
Roman Lazunin <rlazunin@gmail.com>
2016-09-05 12:47:27 +0200

    mvc6 exempel

--- src/WebApplication1/wwwroot/js/site.js
+++ src/WebApplication1/wwwroot/js/site.js
@@ -1,15 +1,34 @@
 ﻿// site.js
 (function (name) {
 
-    var ele = document.getElementById("username");
-    ele.innerHTML = name;
+    var ele = $("#username");
+    ele.text(name);
 
-    var main = document.getElementById("main");
-    main.onmouseenter = function () {
-        main.style.backgroundColor = "#888";
-    };
+    var main = $("#main");
+    main.on("mouseenter", function () {
+        main.style = "background-color: #888;";
+    });
 
-    main.onmouseleave = function () {
-        main.style.backgroundColor = "";
-    };
+    main.on("mouseleave", function () {
+        main.style = "";
+    });
+
+    var menuItems = $("ul.menu li a");
+
+    menuItems.on("click", function () {
+        var me = $(this);
+        alert(me.text());
+    });
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+    });
+
 })("Roman Lazunin");
