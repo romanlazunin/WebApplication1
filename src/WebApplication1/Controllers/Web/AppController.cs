e431c10bd2fd9dbde5d55088088fe351c3225953
Roman Lazunin <rlazunin@gmail.com>
2016-09-07 09:59:24 +0200

    Debug logging och DI

--- src/WebApplication1/Controllers/Web/AppController.cs
+++ src/WebApplication1/Controllers/Web/AppController.cs
@@ -4,11 +4,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebApplication1.Services;
+using WebApplication1.ViewModels;
 
 namespace WebApplication1.Controllers.Web
 {
     public class AppController : Controller
     {
+        private IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -16,6 +25,14 @@ namespace WebApplication1.Controllers.Web
 
         public IActionResult Contact()
         {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            _mailService.SendMail("rlazunin@gmail.com", model.Email, "Contact form", $"{model.Message}");
             return View();
         }
 
--- src/WebApplication1/Startup.cs
+++ src/WebApplication1/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using WebApplication1.Services;
 
 namespace WebApplication1
 {
@@ -16,6 +17,8 @@ namespace WebApplication1
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IMailService, DebugMailService>();
+
             services.AddMvc();
         }
 
