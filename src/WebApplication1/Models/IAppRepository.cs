06c37d582b8c8813b09c26354e9bf6a2deb3c7a1
Roman Lazunin <rlazunin@gmail.com>
2016-09-16 14:39:53 +0200

    view model, AutoMapper, forth and reverse mapping, mapping configuration, async api actions, db repo and context save changes async, includes, related items get and post (Stops)

--- src/WebApplication1/Models/IAppRepository.cs
+++ src/WebApplication1/Models/IAppRepository.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace WebApplication1.Models
 {
     public interface IAppRepository
     {
         IEnumerable<Trip> GetAllTrips();
+        Trip GetTripByName(string tripName);
+        void AddTrip(Trip trip);
+        void AddStop(string tripName, Stop newStop);
+        Task<bool> SaveChangesAsync();
+        
     }
 }
