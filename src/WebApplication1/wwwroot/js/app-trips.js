adfabaa2ee33289c0e270bafe002ae7026050326
Roman Lazunin <rlazunin@gmail.com>
2016-10-03 17:13:48 +0200

    new route and view for trip editor

--- src/WebApplication1/wwwroot/js/app-trips.js
+++ src/WebApplication1/wwwroot/js/app-trips.js
@@ -3,6 +3,21 @@
 
     "use strict";
 
-    angular.module("app-trips", ["simpleControls"]);
+    angular.module("app-trips", ["simpleControls", "ngRoute"])
+        .config(function ($routeProvider) {
+            $routeProvider.when("/", {
+                controller: "tripsController",
+                controllerAs: "vm",
+                templateUrl: "/views/tripsView.html"
+            });
+
+            $routeProvider.when("/editor", {
+                controller: "tripEditorController",
+                controllerAs: "vm",
+                templateUrl: "/views/tripEditorView.html"
+            });
+
+            $routeProvider.otherwise({ redirectTo: "/" });
+        });
 
 })();
